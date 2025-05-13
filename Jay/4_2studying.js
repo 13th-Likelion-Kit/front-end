f606c72122f5078453e12d6bccb1b7e08488618a
JiYi Kim <20230299@kumoh.ac.kr>
2025-05-13 20:29:57 +0900

    4_work

---
+++ Jay/4_2studying.js
@@ -0,0 +1,11 @@
+function change() {
+    const newtext = document.getElementById("text");
+    newtext.textContent = "어려워요";
+}
+
+
+// const newtext = document.getElementById("text");
+// const btn = document.querySelector("button");
+// btn.addEventListener("click", function () {
+//    newtext.textContent = "할 수 있어요.";
+// });
