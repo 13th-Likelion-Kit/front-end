ec0cdadaa8efa8a5e47bf17237fb3de577be0a90
s1hwa <142314061+s1hwa@users.noreply.github.com>
2025-05-13 20:02:48 +0900

    4주차 과제제

---
+++ script.js
@@ -0,0 +1,6 @@
+function changeText() {
+  document.getElementById("text").innerText = "적당히 바람이 시원해 기분이 너무 좋아요 유후";
+ document.querySelector("h1").setAttribute("style", "color: blue;");
+ document.querySelector(".btn").style.backgroundColor = "#d0e7ff";
+  document.querySelector(".btn").style.border = "3px solid #d0e7ff";
+}
