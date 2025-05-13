d3c8f470f2f54db05e19baa817b415870d38e2e1
uzinkkk <yj051888@kumoh.ac.kr>
2025-05-13 20:28:31 +0900

    🤦🏻‍♂️

---
+++ java/java.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("Button1");
+button.addEventListener("click", function () {
+    zin();
+});
+
+const zin = function () {
+  const uzin = document.getElementById("uzinn");
+  uzin.textContent = "저는 말하는 감자임용";
+};
