5251a934aba9122fa9665f10cb4ece74b1449a20
yunadada <ynson0216@naver.com>
2025-05-13 19:53:28 +0900

    과제3 제출

---
+++ work03/work03.js
@@ -0,0 +1,7 @@
+function onButtonClick() {
+  document.getElementById("text").innerHTML = "저는 말하는 감자에요.";
+}
+
+function onButtonClick_Reverse() {
+  document.getElementById("text").innerHTML = "안녕하세요!";
+}
